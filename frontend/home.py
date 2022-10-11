35078c10d8d80599a4d81ed79a639c255420ac13
fabischw <107812436+fabischw@users.noreply.github.com>
2022-10-11 21:30:32 +0200

    fixing deployment issues #3

--- frontend/home.py
+++ frontend/home.py
@@ -4,14 +4,16 @@
 import PIL
 import streamlit as st
 import frontend_funcs
+import pathlib
 
 
+here = pathlib.Path(__file__)
 
-
-
+icon_path_jmp1 = here / 'resources'
+icon_path_fnl = icon_path_jmp1 / 'page_icon.ico'
 
 #set the page icon and title
-icon_load = PIL.Image.open("resources/page_icon.ico")
+icon_load = PIL.Image.open(icon_path_fnl)
 st.set_page_config(page_title="Notenrechner light home",page_icon=icon_load)
 
 
