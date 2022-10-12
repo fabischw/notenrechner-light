c73daee6c5e168a24656eef6bce72a94b26d3579
fabischw <107812436+fabischw@users.noreply.github.com>
2022-10-12 10:26:31 +0200

    testing streamlit deployment issues

--- frontend/home.py
+++ frontend/home.py
@@ -12,10 +12,14 @@ here = pathlib.Path(__file__)
 icon_path_jmp1 = here / 'resources'
 icon_path_fnl = icon_path_jmp1 / 'page_icon.ico'
 
-#set the page icon and title
-icon_load = PIL.Image.open(icon_path_fnl)
-st.set_page_config(page_title="Notenrechner light home",page_icon=icon_load)
 
+try:
+    #set the page icon and title
+    icon_load = PIL.Image.open(icon_path_fnl)
+    st.set_page_config(page_title="Notenrechner light home",page_icon=icon_load)
+except:
+    st.write(here)
+    st.write(icon_path_fnl)
 
 
 
