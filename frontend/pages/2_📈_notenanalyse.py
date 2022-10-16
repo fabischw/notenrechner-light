576a649aa4bb5c7bea447d6e043a6f238871c5a6
fabischw <107812436+fabischw@users.noreply.github.com>
2022-10-16 10:54:29 +0200

    multiple small changes, commiting to move on to required testing

--- frontend/pages/2_📈_notenanalyse.py
+++ frontend/pages/2_📈_notenanalyse.py
@@ -1,7 +1,22 @@
+#importing modules
 import streamlit as st
 import PIL
 import pathlib
 import pandas as pd
+import sys
+
+
+#importing local modules
+here = pathlib.Path(__file__)
+glue_layer = here.parent.parent.parent / "glue"
+
+
+data_core = glue_layer / "data_core.py"
+sys.path.append(data_core)
+import data_core # ! testing required !
+
+
+
 
 #set the page icon
 here = pathlib.Path(__file__)
@@ -21,7 +36,10 @@ st.sidebar.success("Funktion / Modul wählen")
 st.title("Notenanalyse")
 st.markdown("## Diese Funktion dient zur Analyse der Noten-Daten")
 
-st.markdown("### Hier 'Noten.csv' hochladen.")
+#starting up the datacore
 
-#noten_data = st.file_uploader("Notendaten hochladen",type="csv",help="Hier Noten.csv hinziehen, Wenn Sie dieses Programm noch nie genutzt haben, drücken Sie auf 'Datensatz erstellen'.")
+
+
+if st.button("Rohdaten ansehen"):
+    st.table()
 
---
+++ frontend/pages/6_Einstellungen.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import PIL
+import pathlib
+
+#set the page icon
+here = pathlib.Path(__file__)
+resources_dir = here.parent.parent / 'resources'
+icon = resources_dir = resources_dir / 'page_icon.ico'
+icon_load = PIL.Image.open(icon)
+#set the page title
+st.set_page_config(page_title="Einstellungen",page_icon=icon_load)
+
+
+#sidebar
+st.sidebar.success("Funktion / Modul wählen")
+
+
+
+
+st.markdown("## Einstellungen")
