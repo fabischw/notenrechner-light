cf248190680536f4309c802e0540b11262ba232d
fabischw <107812436+fabischw@users.noreply.github.com>
2022-10-25 14:03:51 +0200

    fixed issue in problems folder, implementation pending

--- tests/problems/reproduce/stradio_change_bug/pages/1_repr.py
+++ tests/problems/reproduce/stradio_change_bug/pages/1_repr.py
@@ -14,10 +14,13 @@ number_inpt_choice = None
 
 # function to update the settings
 def update_settings(type):
+    # st.session_state.number_inpt_choice
     st.markdown("this should only run on change!!")# ! debug statement -> this should only be outputted when the value is actually changed !
     if type == "number_input":#updating the number input
-        if number_inpt_choice != st.session_state["inpt_prefered"] and number_inpt_choice != None:
-            st.session_state["inpt_prefered"] = number_inpt_choice
+        st.markdown("another debug statement") # ! debug statement
+        if st.session_state.number_inpt_choice != st.session_state["inpt_prefered"] and number_inpt_choice != None:
+            st.markdown("value changed") # ! debug statement
+            st.session_state["inpt_prefered"] = st.session_state.number_inpt_choice
 
 
 
@@ -29,7 +32,7 @@ choose_prefered_inpt_setting = True
 
 if st.session_state.get("inpt_prefered") == "slider":
     current_choice = 1
-elif st.session_state.get("inpt_prefered") == "inpt_box":
+elif st.session_state.get("inpt_prefered") == "input_box":
     current_choice = 0
 else:
     choose_prefered_inpt_setting = False
@@ -38,4 +41,4 @@ else:
 
 # user choosing input settings
 if choose_prefered_inpt_setting:
-    number_inpt_choice = st.radio("Choose an input method",("input_box","slider"),index=current_choice,on_change=update_settings("number_input"))
+    number_inpt_choice = st.radio("Choose an input method",("input_box","slider"),index=current_choice,on_change=update_settings, args=("number_input",),key="number_inpt_choice")
