a02fc6346d87b3da3c88e77f5bafc0ef0f99161e
fabischw <107812436+fabischw@users.noreply.github.com>
2022-10-25 21:14:19 +0200

    saving changes to data_reader.py

--- glue/data_reader.py
+++ glue/data_reader.py
@@ -10,6 +10,8 @@ import importlib.util
 import sys
 import json
 import os.path
+import streamlit as st
+import copy
 
 
 #paths
@@ -99,6 +101,82 @@ def read_data(target: str):
 
 
 
+def validate_inpt_13(data_dict: dict, comp_arr: list, req_arr: list, data_type: list, extra_conditions: list):
+    # ! requires testing
+    """
+    This function is a component of the write_data function (step 1.3)
+
+    It takes a dictionairy and an  comparison array(comp_arr) which contains strings as well as a required array (req_arr)  which also contains strings
+
+
+    The function checks if all the strings in the comp_arr are keys in the data_dict and if the length of all the keys is the same and if the values in the array for the keys in req_arr are not None
+
+    If the case given above is True, the function returns True, otherwise it returns False
+
+
+    inpt_explanation:
+    data_dict: a dictionairy containing keys, assigned to each key is an array
+    comp_arr: a array containing all the keys which should be present in the data_dict
+    req_arr: a array containing all the keys whose assigned array should always have a value instead of None
+    data_type: datatype at each column (array), must be 'synced' with comp_arr
+
+    extra_conditions: contains special conditions, must be 'synced' with comp_arr, condition representation as an array itself # ? not implemented yet
+
+
+    conditions:
+    1. length same as all others
+    2. if in req_arr values in arr not None
+    3. key in the comp_arr
+    4. datatype correct (=> data_type)
+    5. check if all elements in comp_arr are in the dict (if 3 is true and lengths same this is true)
+
+
+    Y. check if extra_condition is met # ? not implemented yet
+
+
+
+    """
+
+
+    #variable for saving the length of the data arrays
+    ex_len = data_dict[comp_arr[0]]# any key can picked here since the length should always be the same
+
+    # ** condition 5:
+    if len(data_dict) != len(comp_arr):
+        return(False)
+
+
+    for keys, values in data_dict.items:
+
+        # ** condition 1:
+        if len(values) != ex_len:
+            return(False)
+
+        # ** condition 2:
+        if keys in req_arr:
+            for elements in values:
+                if elements == None:
+                    return(False)
+
+        # ** condition 3:
+        if keys not in comp_arr:
+            return False
+
+
+        # function to find the index of the current key in the comp_arr:
+        current_key_idx = None
+        for idx,elements in enumerate(comp_arr):
+            if elements == keys:
+                current_key_idx = idx
+
+
+        # ** condition 4:
+        for elements in values:
+            if type(elements) != type(data_type[current_key_idx]):
+                return(False)
+
+
+
 
 
 
@@ -119,11 +197,25 @@ def write_data(target: str,data):
     2. write data
         2.1 write the data to the target file
 
-    3. return success value
+    3. return success value(True / False)
     """
 
 
     # 1. validate data
+
+
+    # 1.1
+
+
+
+    # 1.3
+
+
+    match target[:target.find(".csv")]:
+        case "noten":
+
+            data_copy = copy.deepcopy(data.values)# create a dictionairy with the data contained in the pandas dataframe
+            # ** pass the data_dictionairy to the validate_inpt_13 function
 
     
 
